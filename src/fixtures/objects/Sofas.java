96ed7d0e1710fcdbca483c5341517b8dfbf23bf1
June Greenup <cgreenup2000@gmail.com>
2021-06-23 16:38:37 -0500

    Created Living room, two sofas, and made some changes

---
+++ src/fixtures/objects/Sofas.java
@@ -0,0 +1,47 @@
+package fixtures.objects;
+
+import fixtures.Fixture;
+
+public class Sofas extends Fixture implements Interactive {
+
+	public Sofas() {
+		super(
+				"Two Sofas",
+				"Comfy, but covered in something?",
+				"Despite how comfortable it looked at first, as you get closer you notice it's covered in a strange slime.\n"
+				+ "Not only that, the slime seems to pulsate, as if it had a heart and were breathing.\n"
+				+ "You decide it's probably best not to sit on it"
+		);
+	}
+
+	@Override
+	public String printLongDescription() {
+		String temp = this.longDescription;
+		spoilTheThought();
+		return temp;
+	}
+
+	@Override
+	public String printShortDescription() {
+		return this.shortDescription;
+	}
+
+	@Override
+	public String printName() {
+		return this.name;
+	}
+
+	@Override
+	public void Interact() {
+		System.out.println("You mindlessly start to sit on it, but the pulsating slime \n"
+				+ "gives you the idea that maybe sitting on it would be a very bad idea.");
+		spoilTheThought();
+
+	}
+	
+	private void spoilTheThought() {
+		this.shortDescription = "Very uncomfy";
+		this.longDescription = "This is definitely not comfy looking anymore. In fact, you are quite repulsed by it now.";
+	}
+
+}
