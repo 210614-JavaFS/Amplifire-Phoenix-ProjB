2920097b094531cdf3b4ecc4d6e582f0d126c88c
rblack <blackrmb@yahoo.com>
2021-06-23 17:38:55 -0400

    Rose's update

--- src/fixtures/objects/Mirror.java
+++ src/fixtures/objects/Mirror.java
@@ -22,8 +22,11 @@ public class Mirror extends Fixture implements Interactive {
 
 	@Override
 	public void Interact() {
-		// TODO Auto-generated method stub
+		System.out.println("You touch the mirror.  Your hand sinks in, and it is very cold");
 		
+		this.name = "An otherworldly mirror";
+		this.shortDescription = "You have just met Hong the ghost.  Don't worry, he's nice.";
+		this.longDescription = "You have just put your hand into a portal to another world.  It should be cold.";
 	}
 
 }
